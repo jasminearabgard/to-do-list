d800d0a91fa8f01977b7931b4593c38d618edd5e
jasminearabgard <yasaman.arabgard@outlook.com>
2021-02-12 14:49:06 +0330

    change

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-//get objects
-let input = document.querySelector("#");
-let btn = document.querySelector("#btn");
-let list = document.getElementById("list");
-let el = document.getElementsByTagName("li");
+ //get objects//
 
-//add element
-btn.addEventListener("click", function() {
-    let txt = input.Value;
-    if (txt === "") {
-        alert("you most write some thing");
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = txt;
-        list.insertBefore(li, list.childNodes[0]);
-        input.value = "";
-    }
-})
+ let input = document.querySelector("#todo");
+ let btn = document.querySelector("#btn");
+ let list = document.querySelector("#list");
+ let el = document.getElementsByTagName("li");
 
-//mark done subject
+ //add element//
 
-list.addEventListener("click", function e() {
-    if (e.target.tagName == "LI") {
-        e.target.classList.toggle("checked");
-    }
-})
+ btn.addEventListener("click", function() {
+     let txt = input.value;
+     if (txt === "") {
+         alert("you most write some thing");
+     } else {
+         let li = document.createElement("li");
+         li.innerHTML = txt;
+         list.insertBefore(li, list.childNodes[0]);
+         input.value = "";
+     }
+ })
+
+ //mark done subject//
+
+ list.addEventListener("click", e => {
+     if (e.target.tagName == "LI") {
+         e.target.classList.toggle("checked");
+     }
+ });
